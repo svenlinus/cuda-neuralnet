6b248ec2a5fe6350e6da8c764e1682ebae3889ba
linustoernqvist <linus.tornqvist@triggerbee.com>
2024-12-04 23:02:31 -0800

    made kernels private

--- matrix.h
+++ matrix.h
@@ -11,23 +11,12 @@ typedef struct {
 /** MEMORY management **/
 void initMatrix(Matrix **mat, int rows, int cols);
 void freeMatrix(Matrix *mat);
-__global__ void initRandomData(Matrix *mat, float range);
-__global__ void initZerosData(Matrix *mat);
 void initRandomMatrix(Matrix **mat, int rows, int cols);
 void initZerosMatrix(Matrix **mat, int rows, int cols);
 void getDeviceMatrixData(float *dest, Matrix *source, int n);
 void setDeviceMatrixData(Matrix *dest, float *source, int n);
 
-/** HELPER **/
-__device__ int size(Matrix *mat);
-
 /** MATH **/
-__global__ void matrixMult(Matrix *a, Matrix *b, Matrix *ab);
-__global__ void matrixAdd(Matrix *a, Matrix *b, Matrix *c, int negate);
-__global__ void matrixScale(Matrix *a, float scale, Matrix *b);
-__global__ void hadamardProd(Matrix *a, Matrix *b, Matrix *c);
-__global__ void sigmoid(Matrix *a, Matrix *b);
-__global__ void sigmoidOutputDerivative(Matrix *a, Matrix *b);
 void deviceMatrixMult(Matrix *a, Matrix *b, Matrix *ab, int N);
 void deviceMatrixAdd(Matrix *a, Matrix *b, Matrix *c, int N);
 void deviceMatrixSub(Matrix *a, Matrix *b, Matrix *c, int N);
@@ -40,7 +29,6 @@ void deviceMatrixReduceRows(Matrix *x, Matrix *y, int rows, int cols);
 
 /** TRANSPOSE **/
 void matrixTranpose(Matrix *a, Matrix **b, int arows, int acols);
-__global__ void transpose(Matrix *input, Matrix *output);
 
 /** LOSS **/
 void squareLoss(Matrix *x, float *result, int rows, int cols);
